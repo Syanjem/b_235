e45534663890e04845c38799ce49025d82936f0d
Syanjem <kosiadagiacopelo83@gmail.com>
2026-09-24 10:01:18 +0800

    rename: 1.1

--- BSP/encoder_spi.c
+++ BSP/encoder_spi.c
@@ -23,7 +23,7 @@ void angle_cs_float_from_encoder(float* a_cs)
 }
 
 
-void angleCs_uint16_t_fromEncoder(uint16_t* a_cs)
+void angle_cs_uint16_t_from_encoder(uint16_t* a_cs)
 {
 	// 开启 SPI 片选
     SPI_CSN1_encoder_SELECT();	// 选择 encoder 片选
--- BSP/encoder_spi.h
+++ BSP/encoder_spi.h
@@ -11,7 +11,7 @@
 #define	SPI_MT_READ_CMD2	((uint16_t)0x84FF)
 
 void angle_cs_float_from_encoder(float* a_cs);
-void angleCs_uint16_t_fromEncoder(uint16_t* a_cs);
+void angle_cs_uint16_t_from_encoder(uint16_t* a_cs);
 
 
 
